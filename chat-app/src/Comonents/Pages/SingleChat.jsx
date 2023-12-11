e6be83b2e489c17f7fbae56ce44d4f2950e6300a
Durvesh27 <132134129+Durvesh27@users.noreply.github.com>
2023-12-11 14:31:04 +0530

    Updated

--- chat-app/src/Comonents/Pages/SingleChat.jsx
+++ chat-app/src/Comonents/Pages/SingleChat.jsx
@@ -18,7 +18,7 @@ import { useEffect } from "react";
 import "./../Style.css";
 import ScrollbleChat from "./ScrollbleChat";
 import io from "socket.io-client";
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://chat-app-mern-5je4.onrender.com";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
