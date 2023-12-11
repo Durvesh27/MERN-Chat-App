0ae4d2b87f51b58df350b9d008a86c1a94d9f631
Durvesh27 <132134129+Durvesh27@users.noreply.github.com>
2023-12-11 12:15:26 +0530

    Updated

--- chat-app/src/Comonents/ApiConfig/index.js
+++ chat-app/src/Comonents/ApiConfig/index.js
@@ -2,14 +2,14 @@ import axios from "axios";
 const token = JSON.parse(localStorage.getItem("ChatToken"));
 if (token) {
   var api = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: "https://chat-app-mern-dnzd.onrender.com/api/v1",
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
 } else {
   var api = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: "https://chat-app-mern-dnzd.onrender.com/api/v1",
   });
 }
 export default api;
